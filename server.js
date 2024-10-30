95b2111ba0856e9cd175ec87914ee28dbc58c2c0
Anastassia Medvedeva <144450154+nastjacveto4ek@users.noreply.github.com>
2024-10-30 22:23:36 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.use(express.json());
 
 app.use('/', todoRouter );
 
-app.listen(3000, () => {
-    console.log('Server is running on Port 3000.');
+app.listen(5000, () => {
+    console.log('Server is running on Port 5000.');
 });
